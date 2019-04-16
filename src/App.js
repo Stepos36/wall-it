7ed6763bc753db0dafc3a3674cd46293a85f40bf
Stepan <stepos36@gmail.com>
2019-04-15 21:00:29 -0400

    forex ticker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import ForexTicker from './components/ForexTicker'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className='row'>
+          <div className='col-md-3'></div>
+          <div className='col-md-6'></div>
+          <div className='col-md-3'>
+            <ForexTicker />
+          </div>
+        </div>
       </div>
     );
   }
