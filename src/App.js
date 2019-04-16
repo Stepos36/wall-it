06fe100c66e2972492a2ffba64abc9973e6bae42
EMalley <EMalley998@gmail.com>
2019-04-15 21:12:56 -0400

    rough outline of header/Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
+        <Navbar />
       </div>
     );
   }
