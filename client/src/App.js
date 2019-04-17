0cde3865d4f6e232258fda396bd5731894c16928
boc4life <lheandrew@gmail.com>
2019-04-17 19:32:48 -0400

    re-structured

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import Landing from "./components/Landing"
+import Home from "./components/Home"
+
+class App extends Component {
+  state = {
+    loggedIn: false,
+    userId: ""
+  }
+
+  landingHandler = (id, boolean) => {
+    this.setState({
+      userId: id,
+      loggedIn: boolean
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? <Home /> : 
+        <Landing 
+        handler={this.landingHandler}/>}
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 var db = require("./models");
 
