c5ee185c078ddb21000f7caa8679f9a761be3e2d
boc4life <lheandrew@gmail.com>
2019-04-19 12:53:59 -0400

    started on stocks component. Makes API call on mount, displays daily movement/total value of dow, nasdaq, s&p in jumbotron

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,6 +4,7 @@ import '../../App.css';
 import Navbar from "./../Navbar";
 import Header from "./../Header";
 import Footer from "./../Footer";
+import Stocks from "./../Stocks";
 
 class Home extends Component {
   render() {
@@ -14,7 +15,8 @@ class Home extends Component {
           <div className='col-md-2'>
             <Navbar />
           </div>
-          <div className='col-md-7'></div>
+          <div className='col-md-7'>
+          </div>
           <div className='col-md-3'>
             <ForexTicker />
           </div>
