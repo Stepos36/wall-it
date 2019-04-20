9fd8492c4fe38d25e19a5ad0e00f5a3379378eee
Stepan Nelasov <stepannelasov@Stepans-MacBook-Pro.local>
2019-04-20 13:20:43 -0400

    tradingview widget

--- client/src/components/Watchlist/index.js
+++ client/src/components/Watchlist/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./style.css";
 
 const Watchlist = () => (
-    <ul class="list-group">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
+    <ul className="list-group">
+        <li className="list-group-item">Cras justo odio</li>
+        <li className="list-group-item">Dapibus ac facilisis in</li>
+        <li className="list-group-item">Morbi leo risus</li>
+        <li className="list-group-item">Porta ac consectetur ac</li>
+        <li className="list-group-item">Vestibulum at eros</li>
     </ul>
 )
 
