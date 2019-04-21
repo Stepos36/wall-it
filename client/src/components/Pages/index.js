523e8612ad748e2369e84c1b89de3b27bc4710e8
EMalley <EMalley998@gmail.com>
2019-04-21 10:53:04 -0400

    styling added to login/register page

---
+++ client/src/components/Pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class profile extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is going to be the profile page</p>
+      </div>
+    )
+  }
+}
+
+export default profile
