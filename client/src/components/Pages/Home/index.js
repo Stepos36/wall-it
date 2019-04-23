50a75e71f8e3edfd50c61aa5b25a8afdc5ebd65d
Stepan Nelasov <stepannelasov@Stepans-MacBook-Pro.local>
2019-04-23 15:25:27 -0400

    calculator added

--- client/src/components/Pages/Home/index.js
+++ client/src/components/Pages/Home/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import CurrencyCalc from './../../CurrencyCalc'
+import CurrencyCalc from './../../CurrencyCalc';
+import Calculator from './../../Calculator/Calculator';
 
 export class Home extends Component {
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-6'>
+      <div className='container comtents'>
+        <div className='row contents'>
+          <div className='col-lg-4 col-md-6 col-sm-12'>
             <CurrencyCalc />
+          </div>
+          <div className='col-lg-4 col-md-6 col-sm-12'>
+
+          </div>
+          <div style={{height: 250}} className='col-lg-4 col-md-6 col-sm-12'>
+            <Calculator />
           </div>
         </div>
       </div>
