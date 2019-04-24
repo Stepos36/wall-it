a94c2a4f6941c6e8d85380809ed85d75aacfd244
Stepan Nelasov <stepannelasov@Stepans-MacBook-Pro.local>
2019-04-23 21:57:16 -0400

    calendar/map/video widgets for main page + responsiveness

---
+++ client/src/components/MapWidget/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 40.535007, lng: -74.521335 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 40.535007, lng: -74.521335 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+)
+
+class MapWidget extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
+
+export default MapWidget;
--- client/src/components/Pages/Home/index.js
+++ client/src/components/Pages/Home/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import CurrencyCalc from './../../CurrencyCalc';
 import Calculator from './../../Calculator/Calculator';
-import ReactDOM from "react-dom";
-import { HorizontalBarChart, VerticalBarChart, PieChart } from '@culturehq/charts';
+import Calendar from 'react-calendar'
+import { PieChart } from '@culturehq/charts';
+import MapWidget from "./../../MapWidget"
+import YVideo from "./../../YouTube"
 import  '@culturehq/charts/dist/style.css';
 import  './style.css';
 const getRandomDatum = () => Math.floor(Math.random() * 100);
@@ -21,14 +23,25 @@ export class Home extends Component {
     return (
       <div className='container comtents'>
         <div className='row contents'>
-          <div className='col-lg-4 col-md-6 col-sm-12'>
-            <CurrencyCalc />
+          <div className='col-lg-4 col-md-6 col-sm-12 youtube'>
+            <YVideo />
           </div>
           <div className='col-lg-4 col-md-6 col-sm-12'>
             <PieChart data={data}/>
           </div>
           <div style={{height: 250}} className='col-lg-4 col-md-6 col-sm-12'>
             <Calculator />
+          </div>
+        </div>
+        <div className='row contents2'>
+          <div className='col-lg-4 col-md-6 col-sm-12 calendar'>
+            <Calendar />
+          </div>
+          <div className='col-lg-4 col-md-6 col-sm-12 map'>
+            <MapWidget />
+          </div>
+          <div className='col-lg-4 col-md-6 col-sm-12'>
+            <CurrencyCalc />
           </div>
         </div>
       </div>
