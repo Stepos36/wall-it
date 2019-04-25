d338feec55882950d61501489279ed1ad88c9928
Kaviz101586 <jason.mapou@gmail.com>
2019-04-24 21:45:17 -0400

    Article working and temp styled

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const stockRoutes = require("./stocks")
+const newsRoutes = require("./news")
 
 // Item routes
 router.use("/users", userRoutes);
-router.use("/stocks", stockRoutes)
+router.use("/stocks", stockRoutes);
+router.use("/news", newsRoutes);
 
 //Exporting
 module.exports = router;
