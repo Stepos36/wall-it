41ac980edab28042aaa95ea97d70011e8656d659
boc4life <lheandrew@gmail.com>
2019-04-26 18:59:25 -0400

    watchlist routing added, add to watchlist functionality inserted into stockpanel component

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
-const stockRoutes = require("./stocks")
+const stockRoutes = require("./stocks");
+const watchlistRoutes = require("./watchlist");
 
 // Item routes
 router.use("/users", userRoutes);
-router.use("/stocks", stockRoutes)
+router.use("/stocks", stockRoutes);
+router.use("/watchlist", watchlistRoutes)
 
 //Exporting
 module.exports = router;
