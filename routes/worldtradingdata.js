1516fc6dc125a9c484fb2272ff8aee3b415adacc
boc4life <lheandrew@gmail.com>
2019-04-29 14:04:12 -0400

    fixed some backend api stuff

--- routes/worldtradingdata.js
+++ routes/worldtradingdata.js
@@ -5,7 +5,7 @@ router.route("/")
     .post((req, res) => {
         let symbols = req.body.symbols
         let query = 'https://www.worldtradingdata.com/api/v1/stock?symbol='+symbols+
-        '&api_token=Dvk9Bf1Qefk0H0z2th3l0Gge2ZafA8FvHjc1MRDhEmJSiffj3Lk0M85AihJ8'
+        '&api_token=PpDqdrjyhNdLa3TQvX7PVlVkCD8BIErUHnOEolFo201hQCf9WD5Fc9fvOWOB'
 
         axios.get(query)
         .then(result => {
