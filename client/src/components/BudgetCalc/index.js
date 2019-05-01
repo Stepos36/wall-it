1adb4501ec9ff5c9856105023b5d4d7ebfb71184
EMalley <EMalley998@gmail.com>
2019-05-01 17:58:12 -0400

    stylized calc page... sorta

--- client/src/components/BudgetCalc/index.js
+++ client/src/components/BudgetCalc/index.js
@@ -11,18 +11,18 @@ import { PieChart } from '@culturehq/charts';
 import '@culturehq/charts/dist/style.css';
 
 class BudgetCalc extends Component {
-    constructor() { 
+    constructor() {
         super();
 
         this.state = {
-        incomeRows: [{"type": "", "value": ""}],
-        expenseRows: [{"type": "", "value": "", "paydate": ""}],
-        userIncomes: [],
-        userExpenses: [],
-        sumIncomes: 0,
-        sumExpenses: 0,
-        netResult: 0,
-        data: {},
+            incomeRows: [{ "type": "", "value": "" }],
+            expenseRows: [{ "type": "", "value": "", "paydate": "" }],
+            userIncomes: [],
+            userExpenses: [],
+            sumIncomes: 0,
+            sumExpenses: 0,
+            netResult: 0,
+            data: {},
         };
 
         this.updateIncomeValues = this.updateIncomeValues.bind(this);
@@ -34,19 +34,19 @@ class BudgetCalc extends Component {
         this.calculateIncExpDiff = this.calculateIncExpDiff.bind(this);
     };
 
-        componentWillMount() {
+    componentWillMount() {
         this.getUserIncomes()
         this.getUserExpenses()
         console.log("Mounted!")
-        }
+    }
 
-        componentDidMount() {
-            console.log(this.state)
-            // this.calculateIncExpDiff()
-        }
+    componentDidMount() {
+        console.log(this.state)
+        // this.calculateIncExpDiff()
+    }
 
-        getUserIncomes() {
-            axios.get("/api/budget/income/" + this.props.userId)
+    getUserIncomes() {
+        axios.get("/api/budget/income/" + this.props.userId)
             .then(response => {
                 if (response.status === 204) {
                     console.log("204")
@@ -59,10 +59,10 @@ class BudgetCalc extends Component {
                     })
                 }
             })
-        }
+    }
 
-        getUserExpenses() {
-            axios.get("/api/budget/expenses/" + this.props.userId)
+    getUserExpenses() {
+        axios.get("/api/budget/expenses/" + this.props.userId)
             .then(response => {
                 if (response.status === 204) {
                     console.log("204")
@@ -76,140 +76,146 @@ class BudgetCalc extends Component {
                     })
                 }
             })
-        }
-        
-        addIncomeRow(event) {
-            event.preventDefault();
-            var rowsIncome = [...this.state.incomeRows]
-            rowsIncome.push(this.state.incomeKey)
-            this.setState({incomeRows: rowsIncome})
-        }
+    }
 
-        addExpenseRow(event) {
-            event.preventDefault();
-            var rowsExpense = [...this.state.expenseRows]
-            rowsExpense.push(this.state.expenseKey)
-            this.setState({expenseRows: rowsExpense})
-            console.log(this.state)
-        }
-        
-        pushIncome(event) {
-            event.preventDefault();
-            console.log(this.props.userId)
-            axios.post("/api/budget/income/" + this.props.userId, {data: this.state.incomeRows})
+    addIncomeRow(event) {
+        event.preventDefault();
+        var rowsIncome = [...this.state.incomeRows]
+        rowsIncome.push(this.state.incomeKey)
+        this.setState({ incomeRows: rowsIncome })
+    }
+
+    addExpenseRow(event) {
+        event.preventDefault();
+        var rowsExpense = [...this.state.expenseRows]
+        rowsExpense.push(this.state.expenseKey)
+        this.setState({ expenseRows: rowsExpense })
+        console.log(this.state)
+    }
+
+    pushIncome(event) {
+        event.preventDefault();
+        console.log(this.props.userId)
+        axios.post("/api/budget/income/" + this.props.userId, { data: this.state.incomeRows })
             .then(response => {
                 console.log(response)
-                let incomeArr = response.data
-                    this.setState({
-                        userIncomes: incomeArr
-                    }, () => {
-                        this.calculateIncExpDiff();
-                    })
-
-            })
-        }
-
-        pushExpenses(event) {
-            event.preventDefault();
-            axios.post("/api/budget/expenses/" + this.props.userId, {data: this.state.expenseRows})
-            .then(response => {
-                console.log(response)
-                let expenseArr = response.data
-                    this.setState({
-                        userExpenses: expenseArr
-                    }, () => {
-                        this.calculateIncExpDiff(); 
-                    })
-            })
-        }
-
-        deleteIncomeRow(id) {
-            axios.put("/api/budget/income/" +this.props.userId, {id:id}).then(response => {
                 let incomeArr = response.data
                 this.setState({
                     userIncomes: incomeArr
+                }, () => {
+                    this.calculateIncExpDiff();
                 })
-            })
-        }
 
-        deleteExpenseRow(id) {
-            axios.put("/api/budget/expenses/" +this.props.userId, {id:id}).then(response => {
+            })
+    }
+
+    pushExpenses(event) {
+        event.preventDefault();
+        axios.post("/api/budget/expenses/" + this.props.userId, { data: this.state.expenseRows })
+            .then(response => {
+                console.log(response)
                 let expenseArr = response.data
                 this.setState({
                     userExpenses: expenseArr
+                }, () => {
+                    this.calculateIncExpDiff();
                 })
-            })            
-        }
-
-        updateIncomeValues(key, value) {
-            this.setState({
-                incomeRows: update(this.state.incomeRows, {[key]: {$set: value}})
             })
-        }
+    }
 
-        updateExpenseValues(key, value) {
+    deleteIncomeRow(id) {
+        axios.put("/api/budget/income/" + this.props.userId, { id: id }).then(response => {
+            let incomeArr = response.data
             this.setState({
-                expenseRows: update(this.state.expenseRows, {[key]: {$set: value}})
+                userIncomes: incomeArr
             })
-        }
+        })
+    }
 
-        calculateIncExpDiff() {
-            var obj = {};
-            var totalIncome = 0;
-            var totalExpense = 0;
-            var netResult = 0;
-            this.state.userIncomes.forEach(item => {
-                totalIncome += parseFloat(item.value)
-            });
-            this.state.userExpenses.forEach(item => {
-                totalExpense += parseFloat(item.value)
-                obj[item.type]=item.value
-            });
-            netResult=totalIncome-totalExpense
+    deleteExpenseRow(id) {
+        axios.put("/api/budget/expenses/" + this.props.userId, { id: id }).then(response => {
+            let expenseArr = response.data
             this.setState({
-                sumIncomes: totalIncome,
-                sumExpenses: totalExpense,
-                netResult: netResult,   
-                data: obj,         
+                userExpenses: expenseArr
             })
-        }
+        })
+    }
 
-        render() {
-            return (
+    updateIncomeValues(key, value) {
+        this.setState({
+            incomeRows: update(this.state.incomeRows, { [key]: { $set: value } })
+        })
+    }
+
+    updateExpenseValues(key, value) {
+        this.setState({
+            expenseRows: update(this.state.expenseRows, { [key]: { $set: value } })
+        })
+    }
+
+    calculateIncExpDiff() {
+        var obj = {};
+        var totalIncome = 0;
+        var totalExpense = 0;
+        var netResult = 0;
+        this.state.userIncomes.forEach(item => {
+            totalIncome += parseFloat(item.value)
+        });
+        this.state.userExpenses.forEach(item => {
+            totalExpense += parseFloat(item.value)
+            obj[item.type] = item.value
+        });
+        netResult = totalIncome - totalExpense
+        this.setState({
+            sumIncomes: totalIncome,
+            sumExpenses: totalExpense,
+            netResult: netResult,
+            data: obj,
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <div className="container">
-                    <div className="row text-center"> 
+                    <div className="row text-center">
                         <div className='col-8'>
                             <div className='col-12'>
-                                Incomes
+                                <h6>Incomes</h6>
                                 <form>
-                                    {this.state.incomeRows.map((row, index) => <FormIncome valueHandler={this.updateIncomeValues} number={index} key={index}/>)}
-                                    <button id="submit" onClick={this.pushIncome}>Submit</button>  
+                                    {this.state.incomeRows.map((row, index) => <FormIncome valueHandler={this.updateIncomeValues} number={index} key={index} />)}
+                                    <button id="submit" onClick={this.pushIncome}>Submit</button>
                                     <button id="addBtn" onClick={(event) => this.addIncomeRow(event)}>Add another income source</button>
                                 </form>
                             </div>
                         </div>
-                    
+
                         <div className='col-4'>
-                           Net Income:{this.state.sumIncomes} <br />
-                           Net Expenses: {this.state.sumExpenses} <br /><br />
-                           TOTAL DIFFERENCE: {this.state.netResult}
+                            <p>
+                                Net Income:{this.state.sumIncomes} <br />
+                            </p>
+                            <p>
+                                Net Expenses: {this.state.sumExpenses} <br /><br />
+                            </p>
+                            <p>
+                                TOTAL DIFFERENCE: {this.state.netResult}
+                            </p>
                         </div>
-                    
+
                     </div>
-                    
+
                     <div className="row text-center">
                         <div className="col-8">
                             <div className='col-12'>
-                                Expenses
+                                <p>Expenses</p>
                                 <form>
-                                    {this.state.expenseRows.map((row, index) => <FormGroup valueHandler={this.updateExpenseValues} number={index} key={index}/>)}
-                                    <button id="submit2" onClick={this.pushExpenses}>Submit</button>  
+                                    {this.state.expenseRows.map((row, index) => <FormGroup valueHandler={this.updateExpenseValues} number={index} key={index} />)}
+                                    <button id="submit2" onClick={this.pushExpenses}>Submit</button>
                                     <button id="addBtn2" onClick={(event) => this.addExpenseRow(event)}>Add another bill</button>
-                                </form>                            
+                                </form>
                             </div>
                         </div>
-                    
+
                         <div className='col-4'>
                             <PieChart data={this.state.data} />
                         </div>
@@ -224,32 +230,32 @@ class BudgetCalc extends Component {
                                 <div className="row col-12 text-center">
                                     <div className="incomeHead container-fluid">
                                         <p className="text-center">
-                                        Your Monthly Net Income
+                                            Your Monthly Net Income
                                         </p>
                                     </div>
                                     <table className="table" id="incomeTable">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Description</th>
-                                            <th scope="col">Amount</th>
-                                        </tr>
-                                    </thead>
-                                    
-                                    <tbody>
-                                    {this.state.userIncomes.map((userIncome, key) => (
-                                        <BudgetIncomeTableRow 
-                                        type={userIncome.type}
-                                        value={userIncome.value}
-                                        remove={this.deleteIncomeRow}
-                                        id={userIncome.id}
-                                        key={userIncome.id}
-                                        />
-                                    ))}
-                                    </tbody>
-                                </table>
-                                </div>
-                            </div>
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Description</th>
+                                                <th scope="col">Amount</th>
+                                            </tr>
+                                        </thead>
 
+                                        <tbody>
+                                            {this.state.userIncomes.map((userIncome, key) => (
+                                                <BudgetIncomeTableRow
+                                                    type={userIncome.type}
+                                                    value={userIncome.value}
+                                                    remove={this.deleteIncomeRow}
+                                                    id={userIncome.id}
+                                                    key={userIncome.id}
+                                                />
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
                             <div className='col-4'>
                                 {/* <PieChart data={this.expenseArr} /> */}
                             </div>
@@ -259,43 +265,43 @@ class BudgetCalc extends Component {
                             <div className='col-8 jumbotron'>
                                 <div className="expenseHead container-fluid">
                                     <p className="text-center">
-                                    Your Monthly Expenses
+                                        Your Monthly Expenses
                                     </p>
                                 </div>
                                 <table className="table" id="expenseTable">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Amount</th>
-                                        <th scope="col">Pay Date</th>
-                                    </tr>
-                                </thead>
-                                
-                                <tbody>
-                                {this.state.userExpenses.map((userExpense,key) => (
-                                    <BudgetExpenseTableRow 
-                                    type={userExpense.type}
-                                    value={userExpense.value}
-                                    paydate={userExpense.paydate}
-                                    remove={this.deleteExpenseRow}
-                                    id={userExpense.id}
-                                    key={userExpense.id}
-                                    />
-                                ))}
-                                </tbody>
-                            </table>
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Amount</th>
+                                            <th scope="col">Pay Date</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        {this.state.userExpenses.map((userExpense, key) => (
+                                            <BudgetExpenseTableRow
+                                                type={userExpense.type}
+                                                value={userExpense.value}
+                                                paydate={userExpense.paydate}
+                                                remove={this.deleteExpenseRow}
+                                                id={userExpense.id}
+                                                key={userExpense.id}
+                                            />
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
 
                             <div className='col-4'></div>
                         </div>
 
                     </div>
-                    
-                    
-                </div>  
+
+
+                </div>
             </div>
-            )
-        }
-        
+        )
+    }
+
 }
 export default BudgetCalc
