a1e9f74aec6e551c6183a9c9516a5b6b2e617e12
boc4life <lheandrew@gmail.com>
2019-05-23 15:52:02 -0400

    removed force true from server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("client/build"));
 
 app.use(routes);
 
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
