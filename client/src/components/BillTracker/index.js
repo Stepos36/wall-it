c67fe795a81c611db8ef3d2d81b24ccd103a9f87
boc4life <lheandrew@gmail.com>
2019-06-01 20:19:54 -0400

    bill tracker added to home page, logic for calculating days between mostly complete. Haven't built display component yet

--- client/src/components/BillTracker/index.js
+++ client/src/components/BillTracker/index.js
@@ -12,16 +12,48 @@ class BillTracker extends Component {
     }
 
     componentDidMount() {
-        axios.get("/api/expenses/" + this.props.userId)
+        axios.get("/api/budget/expenses/" + this.props.userId)
         .then(response => {
-
+            if (response.status === 204) {}
+            else {
+            let date = new Date();
+            let dateNum = date.getDate();
+            let billsArr = response.data
+            for (let i = 0; i < billsArr.length; i++) {
+                if (parseInt(billsArr[i].paydate) > parseInt(dateNum)) {
+                    let daysUntil = parseInt(billsArr[i].paydate) - parseInt(dateNum)
+                    billsArr[i].daysUntil = daysUntil
+                }
+                else {
+                    let month = date.getMonth()
+                    let year = date.getFullYear()
+                    let newMonth = month + 1;
+                    let newYear = year
+                    if (month === 11) newYear += 1
+                    let oneDay = 24*60*60*1000; 
+                    let firstDate = new Date(year,month,dateNum);
+                    let secondDate = new Date(newMonth,newYear,billsArr[i].paydate);
+                    let diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+                    billsArr[i].daysUntil = diffDays
+                }
+            }
+            this.setState({bills: billsArr})
+            console.log(billsArr)
+            }
         })
     }
 
     render() {
         return (
             <div className="container">
-
+                <div>Upcoming Bills</div>
+                {this.state.bills.map(bill => (
+                    <BillDisplay 
+                    name={bill.type}
+                    value={bill.value}
+                    paydate={bill.paydate}
+                    daysUntil={bill.daysUntil}/>
+                ))}
             </div>
         )
     }
