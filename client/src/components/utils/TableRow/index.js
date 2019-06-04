ee72734fd36fee7ceb6221427853effa647678c9
EMalley <EMalley998@gmail.com>
2019-06-04 19:06:17 -0400

    more style changes to upcoming bills, and stocks page

--- client/src/components/utils/TableRow/index.js
+++ client/src/components/utils/TableRow/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function TableRow(props) {
   return (
     <tr>
-        <td><span onClick={() => props.expandDetails(props.index, props.symbol)}>{props.symbol}</span></td>
+        <td><span className="symbolDropMenu" onClick={() => props.expandDetails(props.index, props.symbol)}>{props.symbol}</span></td>
         <td>{props.quantity}</td>
         <td>{props.cashflow}</td>
         <td><button className="lightshadow" onClick={() => {props.addStock("add", `${props.id}`, `${props.quantity}`, `${props.symbol}`)}}>Purchased More Shares</button></td>
