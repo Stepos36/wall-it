b9a9061b8632301a1aed4fb8c2b4aa68e8ce0ce7
boc4life <lheandrew@gmail.com>
2019-06-05 19:38:48 -0400

    new api key

--- routes/worldtradingdata.js
+++ routes/worldtradingdata.js
@@ -5,7 +5,7 @@ router.route("/")
     .post((req, res) => {
         let symbols = req.body.symbols
         let query = 'https://www.worldtradingdata.com/api/v1/stock?symbol='+symbols+
-        '&api_token=7j9FnMBpalVytmiowz2gdPdq6lYk2PDNtJUvLTQjxBUqWxnp6Sp5Az3gS9g5'
+        '&api_token=PpDqdrjyhNdLa3TQvX7PVlVkCD8BIErUHnOEolFo201hQCf9WD5Fc9fvOWOB'
         axios.get(query)
         .then(result => {
             res.status(200).json(result.data)
